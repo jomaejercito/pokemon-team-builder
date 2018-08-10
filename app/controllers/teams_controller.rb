024e197561adce9c6f51f5447d83602547254f50
github username <github email address>
2018-08-09 21:04:44 -0400

    Implemented signup/login feature

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,3 +1,12 @@
 class TeamsController < ApplicationController
 
+  get '/teams' do
+    if logged_in?
+      erb :'teams/index'
+    else
+      redirect '/login'
+    end
+  end
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,47 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if logged_in?
+      redirect '/teams'
+    end
+    erb :'/users/signup'
+  end
+
+  post '/signup' do
+    if params[:username].empty? || params[:email].empty? || params[:password].empty?
+      redirect '/signup'
+    elsif !params[:email].match /@/
+      redirect '/signup'
+    else
+      @user = User.create(params)
+      session[:user_id] = @user.id
+      redirect '/teams'
+    end
+  end
+
+  get '/login' do
+    if logged_in?
+      redirect to '/teams'
+    else
+      erb :'users/login'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/teams'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/'
+  end
+
+
 end
